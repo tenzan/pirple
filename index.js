188f83b63eb2e7c1d44e2b706711d8d2959611db
Askarbek Karasasev <askar.karasaev@gmail.com>
2018-10-27 00:36:18 +0900

    Parsing Query Strings

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ var server = http.createServer(function(req, res) {
     var path = parsedUrl.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+    // Get the query string as an object
+    var queryStringObject = parsedUrl.query;
+
     // Get the HTTP Method
     var method = req.method.toLowerCase();
 
@@ -24,7 +27,7 @@ var server = http.createServer(function(req, res) {
     res.end('Hello World!\n')
 
     // Log the request path
-    console.log('Request received on path: '+trimmedPath+ ' with this method: '+method)
+    console.log('Request received on path: '+trimmedPath+ ' with this method: '+method+' and with these query string parameters',queryStringObject);
 
 });
 
